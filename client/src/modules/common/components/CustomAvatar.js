30a963e131f22f5cf96cd2bc7e9e62df89188cf2
cody0203 <congdinh2395@gmail.com>
2020-07-15 16:37:55 +0700

    Frontend: Update lazy load avatar

--- client/src/modules/common/components/CustomAvatar.js
+++ client/src/modules/common/components/CustomAvatar.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { get } from 'lodash';
+import styled from 'styled-components';
 import { Avatar } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
-import LazyLoad from 'react-lazyload';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const CustomAvatar = ({ src, size }) => {
-  return (
-    <LazyLoad height={size}>
-      <Avatar icon={<UserOutlined />} src={src} size={size} />
-    </LazyLoad>
-  );
+  return <LazyLoadImageStyled src={src} size={size} />;
 };
+
+const LazyLoadImageStyled = styled(LazyLoadImage)`
+  display: block;
+  width: ${({ size }) => `${size}px`};
+  height: ${({ size }) => `${size}px`};
+  border-radius: 50%;
+`;
 
 export default CustomAvatar;
