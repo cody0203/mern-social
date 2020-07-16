eae2a5e679ffc9fc7266377b15f056c423cdb06d
cody0203 <congdinh2395@gmail.com>
2020-07-16 09:54:50 +0700

    Both: Move config file

--- server/src/controllers/auth.controllers.js
+++ server/src/controllers/auth.controllers.js
@@ -1,31 +1,29 @@
-import jwt from "jsonwebtoken";
-import get from "lodash/get";
-import expressJwt from "express-jwt";
+import jwt from 'jsonwebtoken';
+import get from 'lodash/get';
+import expressJwt from 'express-jwt';
 
-import User from "../models/user.model";
-import config from "../../../config/config";
+import User from '../models/user.model';
+import config from '../../config/config';
 
 const signIn = async (req, res, next) => {
   try {
-    const { email, password } = get(req, "body");
+    const { email, password } = get(req, 'body');
     const user = await User.findOne({ email });
 
-    const id = get(user, "_id");
-    const userName = get(user, "name");
-    const userEmail = get(user, "email");
+    const id = get(user, '_id');
+    const userName = get(user, 'name');
+    const userEmail = get(user, 'email');
 
     if (!user) {
-      return res.status(401).json({ message: "User not found!" });
+      return res.status(401).json({ message: 'User not found!' });
     }
 
     if (!user.authenticate(password)) {
-      return res
-        .status(401)
-        .json({ message: "Email and password don't match" });
+      return res.status(401).json({ message: "Email and password don't match" });
     }
 
     const token = jwt.sign({ _id: id }, config.jwtSecret, {
-      expiresIn: "1000d",
+      expiresIn: '1000d',
     });
 
     return res.json({
@@ -37,22 +35,22 @@ const signIn = async (req, res, next) => {
       },
     });
   } catch (err) {
-    return res.status(401).json({ message: "Could not sign in" });
+    return res.status(401).json({ message: 'Could not sign in' });
   }
 };
 
 const userInfo = async (req, res, next) => {
-  const auth = get(req, "auth");
-  const authId = get(auth, "_id");
+  const auth = get(req, 'auth');
+  const authId = get(auth, '_id');
 
   if (!auth) {
-    return res.status(401).json({ message: "Unauthorized" });
+    return res.status(401).json({ message: 'Unauthorized' });
   }
 
-  const user = await User.findById(authId).select("name email updated created");
+  const user = await User.findById(authId).select('name email updated created');
 
   if (!user) {
-    return res.status(401).json({ message: "User not found!" });
+    return res.status(401).json({ message: 'User not found!' });
   }
 
   return res.json({
@@ -61,26 +59,25 @@ const userInfo = async (req, res, next) => {
 };
 
 const signOut = (req, res, next) => {
-  res.clearCookie("t");
-  res.status(200).json({ message: "Signed out" });
+  res.clearCookie('t');
+  res.status(200).json({ message: 'Signed out' });
 };
 
 const requireSignIn = expressJwt({
   secret: config.jwtSecret,
-  userProperty: "auth",
+  userProperty: 'auth',
 });
 
 const hasAuthorization = (req, res, next) => {
-  const profile = get(req, "profile");
-  const profileId = get(profile, "_id");
-  const auth = get(req, "auth");
-  const authId = get(auth, "_id");
+  const profile = get(req, 'profile');
+  const profileId = get(profile, '_id');
+  const auth = get(req, 'auth');
+  const authId = get(auth, '_id');
 
-  const authorized =
-    profile && auth && profileId.toString() === authId.toString();
+  const authorized = profile && auth && profileId.toString() === authId.toString();
 
   if (!authorized) {
-    return res.status(403).json({ error: "User is not authorized" });
+    return res.status(403).json({ error: 'User is not authorized' });
   }
 
   next();
--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-import config from '../../config/config';
+import config from '../config/config';
 import app from './express';
 
 mongoose.set('useFindAndModify', false);
