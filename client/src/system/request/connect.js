c45cad96c3ecd217a5270973980d3b6bc7c3651c
cody0203 <congdinh2395@gmail.com>
2020-07-16 10:45:54 +0700

    Frontend: adding no cache to index file, change server url

--- client/src/system/request/connect.js
+++ client/src/system/request/connect.js
@@ -1,15 +1,15 @@
-import requestFactory, { HTTP_STATUS } from "./request";
+import requestFactory, { HTTP_STATUS } from './request';
 
-const request = requestFactory("http://localhost:8080", {
-  commonHeaders: { Accept: "application/json" },
+const request = requestFactory('https://evening-brook-56461.herokuapp.com', {
+  commonHeaders: { Accept: 'application/json' },
 
   interReq: async (config) => {
-    config.headers["Access-Control-Allow-Headers"] = "Authorization";
-    config.headers["Access-Control-Allow-Origin"] = "*";
+    config.headers['Access-Control-Allow-Headers'] = 'Authorization';
+    config.headers['Access-Control-Allow-Origin'] = '*';
   },
 
   interErr: (error) => {
-    if (process.env.NODE_ENV !== "production") {
+    if (process.env.NODE_ENV !== 'production') {
       console.error(error);
     }
   },
