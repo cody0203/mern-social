68638180a10a9f7bda8e9f88b3d21bcf69929c31
cody0203 <congdinh2395@gmail.com>
2020-07-16 09:17:30 +0700

    Frontend: Update display more icon

--- client/src/modules/comment/CommentItem.js
+++ client/src/modules/comment/CommentItem.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState, useRef } from "react";
-import styled from "styled-components";
-import { Dropdown, Menu, Input } from "antd";
-import { useDispatch, useSelector } from "react-redux";
-import { get, includes } from "lodash";
-import { Link } from "react-router-dom";
-import { EllipsisOutlined, LikeFilled } from "@ant-design/icons";
-import CustomAvatar from "../common/components/CustomAvatar";
-import moment from "moment";
+import React, { useEffect, useState, useRef } from 'react';
+import styled from 'styled-components';
+import { Dropdown, Menu, Input } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+import { get, includes } from 'lodash';
+import { Link } from 'react-router-dom';
+import { EllipsisOutlined, LikeFilled } from '@ant-design/icons';
+import CustomAvatar from '../common/components/CustomAvatar';
+import moment from 'moment';
 
-import CustomDeleteConfirmModal from "../common/components/CustomDeleteConfirmModal";
-import CommentField from "./CommentField";
+import CustomDeleteConfirmModal from '../common/components/CustomDeleteConfirmModal';
+import CommentField from './CommentField';
 
-import * as actions from "../../system/store/comment/comment.actions";
+import * as actions from '../../system/store/comment/comment.actions';
 
 const CommentItem = ({
   comment,
@@ -29,31 +29,24 @@ const CommentItem = ({
 
   const [isShortComment, setIsShortComment] = useState(true);
   const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
-  const [isEditCommentInputVisible, setIsEditCommentInputVisible] = useState(
-    false
-  );
-  const [isInlineCommentFieldFocus, setIsInlineCommentFieldFocus] = useState(
-    false
-  );
+  const [isEditCommentInputVisible, setIsEditCommentInputVisible] = useState(false);
+  const [isInlineCommentFieldFocus, setIsInlineCommentFieldFocus] = useState(false);
   const [isCancelAnchorHover, setIsCancelAnchorHover] = useState(false);
-  const [editingValue, setEditingValue] = useState("");
+  const [editingValue, setEditingValue] = useState('');
 
-  const { deleteCommentLoading, deleteReplyLoading } = useSelector((store) =>
-    get(store, "commentReducer")
-  );
+  const { deleteCommentLoading, deleteReplyLoading } = useSelector((store) => get(store, 'commentReducer'));
 
-  const posterId = get(comment, "owner._id");
-  const id = get(comment, "_id");
-  const isFake = get(comment, "isFake");
+  const posterId = get(comment, 'owner._id');
+  const id = get(comment, '_id');
+  const isFake = get(comment, 'isFake');
 
-  const posterName = get(comment, "owner.name");
-  const content = get(comment, "content");
-  const likes = get(comment, "likes");
-  const created = get(comment, "created");
-  const totalLike = get(likes, "length");
+  const posterName = get(comment, 'owner.name');
+  const content = get(comment, 'content');
+  const likes = get(comment, 'likes');
+  const created = get(comment, 'created');
+  const totalLike = get(likes, 'length');
   const isLiked = includes(likes, userId);
 
-  console.log();
   useEffect(() => {
     setEditingValue(content);
   }, [content]);
@@ -65,7 +58,7 @@ const CommentItem = ({
   }, [isEditCommentInputVisible]);
 
   useEffect(() => {
-    if (get(content, "length") > 30) {
+    if (get(content, 'length') > 30) {
       setIsShortComment(false);
     }
   }, [content]);
@@ -100,23 +93,24 @@ const CommentItem = ({
   };
 
   const closeInlineCommentFieldHandler = () => {
+    setEditingValue(content);
     setIsEditCommentInputVisible(false);
     mouseOutCancelAnchorHandler();
   };
 
   const menu = (
     <Menu onClick={menuClickHandler}>
-      <Menu.Item key="0" onClick={openEditCommentInput}>
+      <Menu.Item key='0' onClick={openEditCommentInput}>
         <p>Edit</p>
       </Menu.Item>
-      <Menu.Item key="1" onClick={onDeleteModal}>
+      <Menu.Item key='1' onClick={onDeleteModal}>
         <p>Delete</p>
       </Menu.Item>
     </Menu>
   );
 
   const likeContainer = (
-    <LikeContainerStyled className={`${!isShortComment && "long-comment"}`}>
+    <LikeContainerStyled className={`${!isShortComment && 'long-comment'}`}>
       <LikeIconStyled />
       <LikeCounterStyled>{totalLike}</LikeCounterStyled>
     </LikeContainerStyled>
@@ -133,22 +127,20 @@ const CommentItem = ({
   };
 
   const onChangeInlineCommentFieldHandler = (e) => {
-    const value = get(e, "target.value");
+    const value = get(e, 'target.value');
     setEditingValue(value);
   };
 
   const detectKeyDownHandler = (e) => {
-    const key = get(e, "key");
+    const key = get(e, 'key');
 
-    if (key === "Escape") {
+    if (key === 'Escape') {
       closeInlineCommentFieldHandler();
     }
   };
 
   const editCommentHandler = () => {
-    dispatch(
-      actions.editCommentStart({ id, params: { content: editingValue } })
-    );
+    dispatch(actions.editCommentStart({ id, params: { content: editingValue } }));
     closeInlineCommentFieldHandler();
   };
 
@@ -165,42 +157,34 @@ const CommentItem = ({
           <div>
             <ContentStyled>
               <Content>
-                <PosterNameStyled to={`/user/profile/${posterId}`}>
-                  {posterName}
-                </PosterNameStyled>{" "}
-                {editingValue}
-              </Content>{" "}
-              {totalLike > 0 && <>{likeContainer}</>}{" "}
+                <PosterNameStyled to={`/user/profile/${posterId}`}>{posterName}</PosterNameStyled> {editingValue}
+              </Content>{' '}
+              {totalLike > 0 && <>{likeContainer}</>}{' '}
               {!isFake && userId === posterId && (
                 <Dropdown
                   visible={isDropdownVisible && currentDropdownId === id}
                   overlay={menu}
-                  trigger={["click"]}
+                  trigger={['click']}
                   onVisibleChange={changeDropdownVisibleHandler.bind(this, id)}
                 >
                   <MoreIconStyled
-                    $isVisible={isDropdownVisible}
-                    className={`${!isShortComment ? "long-comment" : ""}`}
+                    $isVisible={isDropdownVisible && currentDropdownId === id}
+                    className={`${!isShortComment ? 'long-comment' : ''}`}
                   />
                 </Dropdown>
               )}
-            </ContentStyled>{" "}
+            </ContentStyled>{' '}
             <CommentActionsContainerStyled>
               {!isFake && (
                 <>
-                  <LikeActionStyled
-                    onClick={likeCommentHandler.bind(this, id)}
-                    $isLiked={isLiked}
-                  >
+                  <LikeActionStyled onClick={likeCommentHandler.bind(this, id)} $isLiked={isLiked}>
                     Like
                   </LikeActionStyled>
                   {!isReply && (
                     <>
                       <span> · </span>
 
-                      <CommentActionStyled onClick={showReplyInput}>
-                        Reply
-                      </CommentActionStyled>
+                      <CommentActionStyled onClick={showReplyInput}>Reply</CommentActionStyled>
                     </>
                   )}
                   <span> · </span>
@@ -209,7 +193,7 @@ const CommentItem = ({
 
               <span>{moment(created).fromNow()}</span>
             </CommentActionsContainerStyled>
-          </div>{" "}
+          </div>{' '}
           {/* {totalLike > 0 && isShortComment && <>{likeContainer}</>} */}
         </ContentContainerStyled>
       )}
@@ -228,7 +212,7 @@ const CommentItem = ({
           <CancelHintStyled>
             {isInlineCommentFieldFocus ? (
               <span>
-                Press Esc to{" "}
+                Press Esc to{' '}
                 <CancelTextAnchorStyled
                   onClick={closeInlineCommentFieldHandler}
                   onMouseOver={mouseOverCancelAnchorHandler}
@@ -238,9 +222,7 @@ const CommentItem = ({
                 </CancelTextAnchorStyled>
               </span>
             ) : (
-              <CancelTextAnchorStyled onClick={closeInlineCommentFieldHandler}>
-                Cancel
-              </CancelTextAnchorStyled>
+              <CancelTextAnchorStyled onClick={closeInlineCommentFieldHandler}>Cancel</CancelTextAnchorStyled>
             )}
           </CancelHintStyled>
         </div>
@@ -249,8 +231,8 @@ const CommentItem = ({
       <CustomDeleteConfirmModal
         visible={isDeleteModalVisible}
         onCancel={closeDeleteModal}
-        title="Delete comment"
-        desc="Confirm to delete this comment?"
+        title='Delete comment'
+        desc='Confirm to delete this comment?'
         onOk={deleteCommentHandler}
         loading={!isReply ? deleteCommentLoading : deleteReplyLoading}
       />
@@ -265,18 +247,18 @@ const CommentStyled = styled.div`
   padding: 0 36px 16px 16px;
 
   &:first-child {
-    padding-top: ${({ $isReply }) => (!$isReply ? "16px" : 0)};
+    padding-top: ${({ $isReply }) => (!$isReply ? '16px' : 0)};
   }
 `;
 
 const CommentActionStyled = styled.span`
-  color: ${({ theme }) => get(theme, "colors.primaryDark")};
+  color: ${({ theme }) => get(theme, 'colors.primaryDark')};
   cursor: pointer;
 `;
 
 const LikeActionStyled = styled(CommentActionStyled)`
-  font-weight: ${({ $isLiked }) => ($isLiked ? "800" : "400")};
-  color: ${({ $isLiked, theme }) => $isLiked && get(theme, "colors.primary")};
+  font-weight: ${({ $isLiked }) => ($isLiked ? '800' : '400')};
+  color: ${({ $isLiked, theme }) => $isLiked && get(theme, 'colors.primary')};
 `;
 
 const ContentContainerStyled = styled.div`
@@ -286,7 +268,7 @@ const ContentContainerStyled = styled.div`
 `;
 
 const PosterNameStyled = styled(Link)`
-  color: ${({ theme }) => get(theme, "colors.primary")};
+  color: ${({ theme }) => get(theme, 'colors.primary')};
   font-weight: 500;
 `;
 
@@ -297,7 +279,7 @@ const ContentStyled = styled.div`
 `;
 
 const Content = styled.p`
-  background-color: ${({ theme }) => get(theme, "colors.background")};
+  background-color: ${({ theme }) => get(theme, 'colors.background')};
   padding: 6px 12px;
   border-radius: 18px;
 `;
@@ -309,7 +291,7 @@ const MoreIconStyled = styled(EllipsisOutlined)`
     top: 50%;
     transform: translateY(-50%);
   }
-  display: ${({ $isVisible }) => ($isVisible ? "block" : "none")};
+  display: ${({ $isVisible }) => ($isVisible ? 'block' : 'none')};
   font-size: 18px;
   cursor: pointer;
   margin-left: 8px;
@@ -340,7 +322,7 @@ const LikeContainerStyled = styled.div`
 `;
 
 const LikeIconStyled = styled(LikeFilled)`
-  background-color: ${({ theme }) => get(theme, "colors.primary")};
+  background-color: ${({ theme }) => get(theme, 'colors.primary')};
   color: white;
   border-radius: 50%;
   padding: 2px;
@@ -361,7 +343,7 @@ const CancelHintStyled = styled.p`
 `;
 
 const CancelTextAnchorStyled = styled.span`
-  color: ${({ theme }) => get(theme, "colors.primary")};
+  color: ${({ theme }) => get(theme, 'colors.primary')};
   cursor: pointer;
   z-index: 10;
 
