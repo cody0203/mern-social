c2dcaa8e49eff7af7579e696336cffe906358d27
cody0203 <congdinh2395@gmail.com>
2021-09-16 19:48:45 +0700

    Update endpoint local

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const config = {
   server:
     !process.env.NODE_ENV || process.env.NODE_ENV === "development"
-      ? "https://evening-brook-56461.herokuapp.com"
+      ? "http://localhost:8080"
       : "https://evening-brook-56461.herokuapp.com",
 };
 
